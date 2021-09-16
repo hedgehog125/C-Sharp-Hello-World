4d574cd17d128ebf959c8c00b31ad778eca28649
Nico Clack <16543008+hedgehog125@users.noreply.github.com>
2021-09-16 15:30:24 +0100

    Started trying to make it cooler but gave up because plain C# is confusing

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -5,13 +5,38 @@ namespace HelloWorld
 {
     class Program
     {
+        public bool waiting = false;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+
+            ///*
+            asyncWait(1.0f);
+            while (waiting)
+            {
+                Console.WriteLine("Hello World!");
+            }
+            Console.WriteLine("A");
+            int x = 0;
+            while (true)
+            {
+                string output = "";
+                for (int i = 0; i < x; i++)
+                {
+                    output += " ";
+                }
+                output += ".";
+                x++;
+                System.Threading.Thread.Sleep(100);
+            }
+            //*/
+
+
             //while (true) { }
 
 
             // https://stackoverflow.com/questions/30947971/prevent-program-from-closing-when-console-window-closes
+            /*
             Thread t = new Thread(new ThreadStart(doNothing));
 
             t.IsBackground = false;
@@ -19,6 +44,24 @@ namespace HelloWorld
             t.SetApartmentState(ApartmentState.STA);
 
             t.Start();
+            */
+        }
+
+        void syncWait()
+        {
+            System.Threading.Thread.Sleep(1000);
+            waiting = false;
+        }
+        void asyncWait(float duration)
+        {
+            Thread t = new Thread(new ThreadStart(syncWait));
+
+            t.IsBackground = false;
+            t.Priority = ThreadPriority.Normal;
+            t.SetApartmentState(ApartmentState.STA);
+
+            t.Start();
+            waiting = true;
         }
 
         static void doNothing()
