da686b2894b788621096c5476f0c1198932b0dac
Nico Clack <16543008+hedgehog125@users.noreply.github.com>
2021-09-16 15:05:07 +0100

    Initial commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //while (true) { }
+
+
+            // https://stackoverflow.com/questions/30947971/prevent-program-from-closing-when-console-window-closes
+            Thread t = new Thread(new ThreadStart(doNothing));
+
+            t.IsBackground = false;
+            t.Priority = ThreadPriority.Normal;
+            t.SetApartmentState(ApartmentState.STA);
+
+            t.Start();
+        }
+
+        static void doNothing()
+        {
+
+        }
+    }
+}
